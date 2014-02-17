2f61ac6e206ca9318e8267f714a368ee15e1877e
Peter van Dijk <peter.van.dijk@netherlabs.nl>
2014-02-17 13:13:32 +0100

    update sql schema

--- schema.sql
+++ schema.sql
@@ -4,11 +4,12 @@ create table domains (
  master          VARCHAR(128) DEFAULT NULL,
  last_check      INT DEFAULT NULL,
  type            VARCHAR(6) NOT NULL,
- notified_serial INT DEFAULT NULL, 
- account         VARCHAR(40) DEFAULT NULL
+ notified_serial INT DEFAULT NULL,
+ account         VARCHAR(40) DEFAULT NULL,
+ CONSTRAINT c_lowercase_name CHECK (((name)::text = lower((name)::text)))
 );
 CREATE UNIQUE INDEX name_index ON domains(name);
-  
+
 CREATE TABLE records (
         id              SERIAL PRIMARY KEY,
         domain_id       INT DEFAULT NULL,
@@ -17,8 +18,9 @@ CREATE TABLE records (
         content         VARCHAR(65535) DEFAULT NULL,
         ttl             INT DEFAULT NULL,
         prio            INT DEFAULT NULL,
-        change_date     INT DEFAULT NULL, 
-        CONSTRAINT domain_exists 
+        change_date     INT DEFAULT NULL,
+        disabled        BOOL DEFAULT NULL,
+        CONSTRAINT domain_exists
         FOREIGN KEY(domain_id) REFERENCES domains(id)
         ON DELETE CASCADE,
         CONSTRAINT c_lowercase_name CHECK (((name)::text = lower((name)::text)))
@@ -29,15 +31,43 @@ CREATE INDEX nametype_index ON records(name,type);
 CREATE INDEX domain_id ON records(domain_id);
 
 create table supermasters (
-          ip VARCHAR(64) NOT NULL, 
-          nameserver VARCHAR(255) NOT NULL, 
-          account VARCHAR(40) DEFAULT NULL
+          ip INET NOT NULL,
+          nameserver VARCHAR(255) NOT NULL,
+          account VARCHAR(40) DEFAULT NULL,
+          PRIMARY KEY (ip, nameserver)
+);
+alter table records add ordername   VARCHAR(255);
+alter table records add auth bool;
+create index recordorder on records (domain_id, ordername text_pattern_ops);
+
+create table domainmetadata (
+ id         SERIAL PRIMARY KEY,
+ domain_id  INT REFERENCES domains(id) ON DELETE CASCADE,
+ kind       VARCHAR(16),
+ content    TEXT
 );
 
--- GRANT SELECT ON supermasters TO pdns;
--- GRANT ALL ON domains TO pdns;
--- GRANT ALL ON domains_id_seq TO pdns;
--- GRANT ALL ON records TO pdns;
--- GRANT ALL ON records_id_seq TO pdns;
+create index domainidmetaindex on domainmetadata(domain_id);
 
-        
+
+create table cryptokeys (
+ id         SERIAL PRIMARY KEY,
+ domain_id  INT REFERENCES domains(id) ON DELETE CASCADE,
+ flags      INT NOT NULL,
+ active     BOOL,
+ content    TEXT
+);
+create index domainidindex on cryptokeys(domain_id);
+
+
+create table tsigkeys (
+ id         SERIAL PRIMARY KEY,
+ name       VARCHAR(255),
+ algorithm  VARCHAR(50),
+ secret     VARCHAR(255),
+ constraint c_lowercase_name check (((name)::text = lower((name)::text)))
+);
+
+create unique index namealgoindex on tsigkeys(name, algorithm);
+
+alter table records alter column type type VARCHAR(10);
